2183f2314e6d2c3acbe9622e3ae7e80adca4c6d1
何伟 <hewei@weimai.com>
2014-10-16 17:35:30 +0800

    update Pascal's Triangle

--- java/Pascal's Triangle/Solution.java
+++ java/Pascal's Triangle/Solution.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
 
     public List<List<Integer>> generate(int numRows) {
-        return null;
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        return result;
     }
 }
