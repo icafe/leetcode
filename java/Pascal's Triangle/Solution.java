30d34fdd5f629037a2ec0879584065b13f5637bc
icafe <gocoder@vip.qq.com>
2014-10-16 17:52:44 +0800

    update Pascal's Triangle

--- java/Pascal's Triangle/Solution.java
+++ java/Pascal's Triangle/Solution.java
@@ -5,6 +5,7 @@ public class Solution {
 
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
+        int value = 0;
         return result;
     }
 }
